d1cefec2fa6145d86a02561bf33f08b3f4048c75
Prabhash Rathore <prabhashrathore@gmail.com>
2014-12-11 00:34:11 -0800

    Anonymous function

---
+++ CodeSnippet/AnonymousFunction.js
@@ -0,0 +1,11 @@
+/**
+ * Created by prrathore on 12/11/14.
+ */
+
+//Call Anonymous function by putting () at the end of anonymous function
+(function() {
+    var x  = 5;
+    var y = 10;
+    console.log(x + " " + y);
+})();
+
