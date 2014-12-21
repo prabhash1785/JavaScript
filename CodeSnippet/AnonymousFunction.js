4f1e3dfda6d7aa7d8a9b1f01f393d35fc1e33686
Prabhash Rathore <prabhashrathore@gmail.com>
2014-12-20 16:59:27 -0800

    anonymous function

--- CodeSnippet/AnonymousFunction.js
+++ CodeSnippet/AnonymousFunction.js
@@ -2,10 +2,19 @@
  * Created by prrathore on 12/11/14.
  */
 
-//Call Anonymous function by putting () at the end of anonymous function
+//Call Anonymous function by putting () at the end of anonymous function to self-execute it immediately
 (function() {
     var x  = 5;
     var y = 10;
+    z = 3; //global variable as it's not declared
     console.log(x + " " + y);
 })();
 
+(function() {
+    console.log("z: " + z); //accessing the global variable z
+})();
+
+
+(function(a, b) {
+    console.log("Sum of a + b = " + (a + b));
+})(5, 7);
