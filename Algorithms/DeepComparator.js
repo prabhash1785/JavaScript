af0eb0f9b7a2dec6a12575e0e17ed6c9ac458f20
Prabhash Rathore <prabhashrathore@gmail.com>
2014-12-24 11:13:28 -0800

    reference equality

--- Algorithms/DeepComparator.js
+++ Algorithms/DeepComparator.js
@@ -27,5 +27,13 @@ var obj2 = {
 };
 
 //check equality of objects using different techniques
-console.log("obj1 == obj2 => " + obj1 == obj2); //false
-console.log("obj1 === obj2 => " + obj1 === obj2); //false
+// === this checks for reference
+var result1 = obj1 == obj2;
+console.log("obj1 == obj2 => " + result1); //false
+
+var result2 = obj1 === obj2;
+console.log("obj1 === obj2 => " + result2); //false
+
+var obj3 = obj1; //now their reference is going to be same
+var result3 = obj3 === obj1;
+console.log("obj3 === obj1 => " + result3);
