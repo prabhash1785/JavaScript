1e75d1681d478f4d372feb67ca5aac196af58332
Prabhash Rathore <prabhashrathore@gmail.com>
2015-02-03 23:20:07 -0800

    Array operations

---
+++ CodeSnippet/ArrayOperations.js
@@ -0,0 +1,12 @@
+/**
+ * Operations based on Array.
+ *
+ * Created by prrathore on 2/3/15.
+ */
+
+//slice method - returns a shallow copy of an array into a new array object
+//new array is from including start index and exclduing last index
+var slicedArray = [4, 2, 6, 9, 1, 81, 3, 28].slice(2, 5);
+console.log("Sliced Array: " + slicedArray);
+
+
