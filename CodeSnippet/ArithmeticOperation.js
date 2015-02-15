4586085d33279420f4e0d62ccbec0753cac9bb86
Prabhash Rathore <prabhashrathore@gmail.com>
2015-02-15 15:02:30 -0800

    Arithmetic operations

---
+++ CodeSnippet/ArithmeticOperation.js
@@ -0,0 +1,13 @@
+/**
+ * Arithmetic Operations
+ *
+ * Created by prrathore on 2/15/15.
+ */
+
+//find exponential power using JS Math library
+var base = 2;
+var power = 32;
+
+var output = Math.pow(base, power);
+
+console.log("Output: " + output);
