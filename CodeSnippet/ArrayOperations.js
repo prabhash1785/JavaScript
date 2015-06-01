14680a0e308884d7da3f19cea4eca46b83785b4d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-06-01 15:36:12 -0700

    Sample utility function on an array using array.map

--- CodeSnippet/ArrayOperations.js
+++ CodeSnippet/ArrayOperations.js
@@ -41,3 +41,55 @@ console.log(things);
 var a = [7, 2, 9, 5].forEach(function(element, index, array) {
     console.log('a[' + index + '] = ' + element);
 });
+
+//iterate through given array of objects and based on the condition create two arrays
+//sample code to solve real project problem where I have split a result into two arrays based on certain conditions and also each
+//object needs to have certain values mapped before pushing to relevant target arrays
+var inputArray = [
+    {name: 'a', status: 'pending'},
+    {name: 'b', status: 'completed'},
+    {name: 'c', status: 'pending'},
+    {name: 'd', status: 'completed'},
+    {name: 'e', status: 'pending'},
+    {name: 'f', status: 'completed'},
+    {name: 'g', status: 'pending'},
+    {name: 'h', status: 'completed'},
+    {name: 'i', status: 'completed'},
+    {name: 'j', status: 'completed'}
+];
+
+var pendingArray = [],
+    completedArray = [];
+
+inputArray.map(function(obj) {
+
+    var mappedObject = {};
+
+    mappedObject[obj.name] = obj.status;
+
+    if(obj.status === 'pending') {
+        pendingArray.push(mappedObject);
+    } else {
+        completedArray.push(mappedObject);
+    }
+
+});
+
+//Print final arrays
+console.log("Input Array:");
+printArrayElements(inputArray);
+
+//Print pending state arrays
+console.log("Pending State Array:");
+printArrayElements(pendingArray);
+
+//Print completed state arrays
+console.log("Completed State Array:");
+printArrayElements(completedArray);
+
+function printArrayElements(list) {
+    for(var i = 0; i < list.length; i++) {
+        console.log(JSON.stringify(list[i]));
+    }
+}
+
