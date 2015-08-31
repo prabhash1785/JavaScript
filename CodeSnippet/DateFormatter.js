b64be95be10721eb4c52da159dfee81fd40729b2
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-31 14:13:19 -0700

    refactoring

--- CodeSnippet/DateFormatter.js
+++ CodeSnippet/DateFormatter.js
@@ -4,29 +4,37 @@
 
 'use strict';
 
-var inputDate = '2015-05-21T05:02:56.000Z';
+/**
+ * Format date
+ *
+ */
+function formatDate() {
+
+    var inputDate = '2015-05-21T05:02:56.000Z';
 
 // parse it to Unix timestamp
-var parsedDate = Date.parse(inputDate);
-console.log('Date: ' + parsedDate);
+    var parsedDate = Date.parse(inputDate);
+    console.log('Date: ' + parsedDate);
 
 // parse date and retrieve Month and Date
-var d = new Date('2015-01-21T05:02:56.000Z');
-console.log('Date object: ' + d); // Wed May 20 2015 22:02:56 GMT-0700 (PDT)
-console.log('Stringified Date: ' + d.toString());  // Wed May 20 2015 22:02:56 GMT-0700 (PDT)
+    var d = new Date('2015-01-21T05:02:56.000Z');
+    console.log('Date object: ' + d); // Wed May 20 2015 22:02:56 GMT-0700 (PDT)
+    console.log('Stringified Date: ' + d.toString());  // Wed May 20 2015 22:02:56 GMT-0700 (PDT)
 
-var dateTokens = d.toString().split(' ');
-console.log('Month: ' + dateTokens[1]);
-console.log('Date: ' + dateTokens[2]);
+    var dateTokens = d.toString().split(' ');
+    console.log('Month: ' + dateTokens[1]);
+    console.log('Date: ' + dateTokens[2]);
 
 //Date Manipulation using MomentJS API
-var moment = require('../node_modules/moment');
+    var moment = require('../node_modules/moment');
 
-var presentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log('Present Date: ' + presentDate);
+    var presentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log('Present Date: ' + presentDate);
 
-var formattedDate = moment('12-25-1995', 'MM-DD-YYYY').format('MMM DD');
-console.log('Formatted Date: ' + formattedDate); //Prints Dec 25
+    var formattedDate = moment('12-25-1995', 'MM-DD-YYYY').format('MMM DD');
+    console.log('Formatted Date: ' + formattedDate); //Prints Dec 25
+
+}
 
 /**
  * Get first date of current month.
@@ -46,7 +54,7 @@ function getFirstDateOfCurrentMonth() {
 
 }
 
-getFirstDateOfCurrentMonth();
+// getFirstDateOfCurrentMonth();
 
 /**
  * Date manipultaion with griffin
